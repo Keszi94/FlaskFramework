dab873a42b04a8e4aa99074bc6510598a800867c
Eszter Kiskarpati <keszi94@gmail.com>
2024-11-30 23:40:57 +0000

    Add base. html and use template inheritance to minimise code in the 3 html pages.

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def contact():
     return render_template("contact.html")        
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
